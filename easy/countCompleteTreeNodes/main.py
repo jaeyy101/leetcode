802223780d9809480c898c52d03915760821943e
jaeyy101 <custofeng159@gmail.com>
2025-05-13 10:36:12 -0400

    Count Nodes

--- easy/countCompleteTreeNodes/main.py
+++ easy/countCompleteTreeNodes/main.py
@@ -1,23 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def countNodes(self, root: Optional[TreeNode]) -> int:
-        def get_height(node):
-            h = 0
-            while node:
-                h += 1
-                node = node.left
-            return h
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
         if not root:
             return 0
-        left_height = get_height(root.left)
-        right_height = get_height(root.right)
+
+        left_height = self.get_height(root, True)
+        right_height = self.get_height(root, False)
+
         if left_height == right_height:
-            return (1 << left_height) + self.countNodes(root.right)
+            return (1 << left_height) - 1
         else:
-            return (1 << right_height) + self.countNodes(root.left)
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+    def get_height(self, node: TreeNode, left):
+        h = 0
+        while node:
+            h += 1
+            node = node.left if left else node.right
+        return h
