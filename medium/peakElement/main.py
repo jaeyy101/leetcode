4d116623b24e6493b92987434bf9aa344e1bb54f
jaeyy101 <custofeng159@gmail.com>
2025-05-23 12:55:10 -0400

    Peak Element

--- medium/peakElement/main.py
+++ medium/peakElement/main.py
@@ -1,25 +1,12 @@
 class Solution:
-    def peakElement(self, nums: list[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return nums[0]
+    def findPeakElement(self, nums: list[int]) -> int:
         left = 0
-        right = n - 1
+        right = len(nums) - 1
 
         while left < right:
             mid = (left + right) // 2
-            bigger_than_latter = nums[mid] > nums[mid + 1]
-            bigger_than_former = nums[mid] > nums[mid - 1]
-
-            if bigger_than_former and bigger_than_latter:
-                return mid
-
-            if bigger_than_former:
-                left = mid + 1
+            if nums[mid] > nums[mid + 1]:
+                right = mid
             else:
-                right = mid - 1
-
-        return left
-
-
-print(Solution().peakElement([1, 2, 1, 3, 5, 6, 8]))
+                left = mid + 1
+        return right
